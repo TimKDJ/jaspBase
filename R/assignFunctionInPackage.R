21e3669953cb477db2a5b022ee9b58316082d874
TimKDJ <tim_jong@hotmail.com>
2020-09-09 11:23:08 +0200

    Overwrite gettext functions to use the correct domain

--- R/assignFunctionInPackage.R
+++ R/assignFunctionInPackage.R
@@ -1,10 +1,10 @@
 assignFunctionInPackage <- function(fun, name, package) {
   #'@title Overwrite functions inside R packages
-  #' 
+  #'
   #'@param fun    : function you want to replace
   #'@param name   : the name of the function in a package
   #'@param package: the name of the package
-  #'  
+  #'
   #' example usage:
   #' avoid a parallel backend from being registered which triggers a firewall message
   ns <- getNamespace(package)
@@ -13,25 +13,25 @@ assignFunctionInPackage <- function(fun, name, package) {
   lockBinding(name, ns)
 }
 
-# NOTE: let's make it a custom to start the new function names with "fake" followed by the name of the original 
+# NOTE: let's make it a custom to start the new function names with "fake" followed by the name of the original
 # function. Also, let's put all "modified" versions of functions in this file. Note that thise file probably needs
 # to be resolved completely for R syntax.
 
 # mlRegressionBoosting ----
-fakeGbmCrossValModelBuild <- function(cv.folds, cv.group, n.cores, i.train, x, y, offset, 
-                                      distribution, w, var.monotone, n.trees, interaction.depth, 
-                                      n.minobsinnode, shrinkage, bag.fraction, var.names, response.name, 
+fakeGbmCrossValModelBuild <- function(cv.folds, cv.group, n.cores, i.train, x, y, offset,
+                                      distribution, w, var.monotone, n.trees, interaction.depth,
+                                      n.minobsinnode, shrinkage, bag.fraction, var.names, response.name,
                                       group) {
   # the first two lines create a parallel backend and trigger a firewall message
   # cluster <- gbmCluster(n.cores)
   # on.exit(parallel::stopCluster(cluster))
   seeds <- as.integer(runif(cv.folds, -(2^31 - 1), 2^31))
-  # parallel::parLapply(cl = NULL, X = 1:cv.folds, fun = gbmDoFold, 
-  #                     i.train, x, y, offset, distribution, w, var.monotone, 
-  #                     n.trees, interaction.depth, n.minobsinnode, shrinkage, 
-  #                     bag.fraction, cv.group, var.names, response.name, group, 
+  # parallel::parLapply(cl = NULL, X = 1:cv.folds, fun = gbmDoFold,
+  #                     i.train, x, y, offset, distribution, w, var.monotone,
+  #                     n.trees, interaction.depth, n.minobsinnode, shrinkage,
+  #                     bag.fraction, cv.group, var.names, response.name, group,
   #                     seeds)
-  
+
   # NOTE: gbm::gbmDoFold calls library(gbm, silent = TRUE) so we make another fake function
   fakeGbmDoFold <- function(X, i.train, x, y, offset, distribution, w, var.monotone,
                             n.trees, interaction.depth, n.minobsinnode, shrinkage, bag.fraction,
@@ -43,15 +43,15 @@ fakeGbmCrossValModelBuild <- function(cv.folds, cv.group, n.cores, i.train, x, y
     offset <- offset[i.train][i]
     nTrain <- length(which(cv.group != X))
     group <- group[i.train][i]
-    res <- gbm::gbm.fit(x = x, y = y, offset = offset, distribution = distribution, 
-                        w = w, var.monotone = var.monotone, n.trees = n.trees, 
-                        interaction.depth = interaction.depth, n.minobsinnode = n.minobsinnode, 
-                        shrinkage = shrinkage, bag.fraction = bag.fraction, nTrain = nTrain, 
-                        keep.data = FALSE, verbose = FALSE, response.name = response.name, 
+    res <- gbm::gbm.fit(x = x, y = y, offset = offset, distribution = distribution,
+                        w = w, var.monotone = var.monotone, n.trees = n.trees,
+                        interaction.depth = interaction.depth, n.minobsinnode = n.minobsinnode,
+                        shrinkage = shrinkage, bag.fraction = bag.fraction, nTrain = nTrain,
+                        keep.data = FALSE, verbose = FALSE, response.name = response.name,
                         group = group)
     res
   }
-  lapply(X = 1:cv.folds, FUN = fakeGbmDoFold, i.train, x, y, offset, distribution, w, var.monotone, n.trees, 
+  lapply(X = 1:cv.folds, FUN = fakeGbmDoFold, i.train, x, y, offset, distribution, w, var.monotone, n.trees,
          interaction.depth, n.minobsinnode, shrinkage, bag.fraction, cv.group, var.names, response.name, group, seeds)
 }
 
@@ -68,13 +68,41 @@ fakeGbmCrossValErr <- function(cv.models, cv.folds, cv.group, nTrain, n.trees) {
 fakeGrDevicesPdf <- function(file = if(onefile) "Rplots.pdf" else "Rplot%03d.pdf",
                              width, height, onefile, family, title, fonts, version,
                              paper, encoding, bg, fg, pointsize, pagecentre, colormodel,
-                             useDingbats, useKerning, fillOddEven, compress) { 
+                             useDingbats, useKerning, fillOddEven, compress) {
   args <- as.list(match.call())
   matchedIdx <- which(names(args) %in% c("file", "width", "height", "oneFile", "family", "bg", "pointsize"))
   matchedArgs <- args[matchedIdx]
-  
+
   matchedArgs[["filename"]] <- matchedArgs[["file"]]
   matchedArgs[["file"]] <- NULL
-  
+
   do.call(grDevices::cairo_pdf, matchedArgs, envir = parent.frame())
+}
+
+fakeGettext <- function(..., domain = NULL) {
+  if (missing(domain)) {
+    pkgName <- packageName(sys.frame(-1))
+    if (!is.null(pkgName))
+      domain <- paste0("R-", pkgName)
+  }
+  args <- lapply(list(...), as.character)
+  .Internal(gettext(domain, unlist(args)))
+}
+
+fakeGettextf <- function(fmt, ..., domain = NULL) {
+  if (missing(domain)) {
+    pkgName <- packageName(sys.frame(-1))
+    if (!is.null(pkgName))
+      domain <- paste0("R-", pkgName)
+  }
+  sprintf(gettext(fmt, domain = domain), ...)
+}
+
+fakeNgettext <- function(n, msg1, msg2, domain = NULL) {
+  if (missing(domain)) {
+    pkgName <- packageName(sys.frame(-1))
+    if (!is.null(pkgName))
+      domain <- paste0("R-", pkgName)
+  }
+  .Internal(ngettext(n, msg1, msg2, domain))
 }
